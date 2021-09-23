8729fb438f062aab6d7e76950468a89440477eb2
Lutz Helm <helm@ub.uni-leipzig.de>
2021-09-23 10:24:51 +0200

    Omit empty seeAlso in Canvas

--- templates/src/main/java/de/ubleipzig/iiifproducer/template/TemplateCanvas.java
+++ templates/src/main/java/de/ubleipzig/iiifproducer/template/TemplateCanvas.java
@@ -19,6 +19,7 @@
 package de.ubleipzig.iiifproducer.template;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
@@ -31,6 +32,7 @@ import java.util.List;
  *
  * @author christopher-johnson
  */
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({"@id", "@type", "label", "height", "width", "images", "seeAlso"})
 public class TemplateCanvas {
 
